c0ee868c2ad776de84eeccb33cbc4a98f15668e9
Divya Indi <divyaindi@Divyas-MacBook-Pro.local>
2015-11-14 18:55:02 -0800

    Mapper for skills

---
+++ mapper_skill.py
@@ -0,0 +1,20 @@
+__author__ = 'divyaindi'
+#!/usr/bin/env python
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    jobs = line.split('##')
+    for job in jobs:
+        skills = job.split('::')
+        skill = skills[2].split(',')
+        for s in skill:
+            s = s.strip()
+            print '%s\t%s#%s' % (s,job,1)
+
+
+
+    
